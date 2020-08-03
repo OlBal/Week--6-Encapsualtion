15c75694cab2aef6c8a59edfcd0e2210511583af
Oliver Ballon <oliverballon1@gmail.com>
2020-08-03 15:33:47 +0100

    challenege 3, array accessed and comapared

---
+++ Exercises/day1/basic_php2/challenge3.php
@@ -0,0 +1,25 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+//Update sameName to return true if the associative arrays have the same name property and false if they have different names.
+
+function sameName ($person1, $person2) {
+    
+    if ($person1["name"] !== $person2["name"]) {
+
+       return false;
+
+    }else return true;
+
+};
+
+$a = [ "name" => "Alice", "age" => 52 ];
+$b = [ "name" => "Bob", "age" => 34 ];
+$c = [ "name" => "Charlie", "age" => 24 ];
+$d = [ "name" => "Bob", "age" => 97 ];
+
+dump(sameName($a, $b)); // false
+dump(sameName($b, $c)); // false
+dump(sameName($c, $d)); // false
+dump(sameName($b, $d)); // true
