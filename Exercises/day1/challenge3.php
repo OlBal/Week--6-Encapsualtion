e0db3708feecad3243c5bea16ccd8c3dec1e72df
Oliver Ballon <oliverballon1@gmail.com>
2020-08-03 11:36:46 +0100

    challenges 3+4

---
+++ Exercises/day1/challenge3.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+//Write some code that lists the numbers from 1 to 100 using a for loop.
+
+
+for($i = 1; $i <= 100; $i += 1){
+    dump ($i);
+}
+
+
---
+++ Exercises/day1/challenge4.php
@@ -0,0 +1,12 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+//Write some code that lists all multiples of 13 up to 1,000 using a for loop
+
+
+for($i = 0; $i <= 1000; $i += 13){
+    dump ($i);
+}
+
+
