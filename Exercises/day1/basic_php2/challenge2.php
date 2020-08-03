2e84dee632999f8ebc708e795548f39f117d2997
Oliver Ballon <oliverballon1@gmail.com>
2020-08-03 15:06:23 +0100

    first challenge interpolated

---
+++ Exercises/day1/basic_php2/challenge2.php
@@ -0,0 +1,8 @@
+<?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+
+//Create a function, joinWords, that takes two strings as arguments. The function should return the two strings joined together with a space in between.
+
+
