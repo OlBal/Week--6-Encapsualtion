79fe438737b71d5a88cb1684d88c4755b171c770
Oliver Ballon <oliverballon1@gmail.com>
2020-08-04 11:59:22 +0100

    regex_5 created, regex_4 unfinished

--- Exercises/day2/regex_3.php
+++ Exercises/day2/regex_3.php
@@ -4,6 +4,11 @@ require __DIR__ . "/vendor/autoload.php";
 
 //Create a function, zOrQ, that returns true if the given string contains the letter q or z (uppercase and lowercase):
 
+function zOrQ($str){
+
+    return preg_match( "/q|z|Z/" ,$str) === 1;
+}
+
 dump(zOrQ("blah")); // false
 dump(zOrQ("blqh")); // true
 dump(zOrQ("bZ3h5")); // true
