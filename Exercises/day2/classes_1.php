8886e3a46c908b27fceadb1f0db0eace5e836568
Oliver Ballon <oliverballon1@gmail.com>
2020-08-04 14:51:05 +0100

    php append added to filename

---
+++ Exercises/day2/classes_1.php
@@ -0,0 +1,17 @@
+?php
+
+require __DIR__ . "/vendor/autoload.php";
+
+// Create a class that represents a phone
+
+// create three phones
+$iPhone = new Phone("Apple", "iPhone XS");
+$galaxy = new Phone("Samsung", "Galaxy");
+$retro = new Phone("Nokia", "3210");
+
+// use methods to find out model/make
+dump($iPhone->model()); // "iPhone XS"
+dump($retro->make()); // "Nokia"
+dump($galaxy->make()); // "Samsung"
+dump($iPhone->make()); // "Apple"
+
